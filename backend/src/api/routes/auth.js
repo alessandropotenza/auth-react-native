785955297f049dd832c1672e4e4784e6b8bccaab
Alessandro Potenza <potenza1702@gmail.com>
2022-07-22 17:47:46 -0500

    clean up code

--- backend/src/api/routes/auth.js
+++ backend/src/api/routes/auth.js
@@ -5,13 +5,6 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 
 // routes
-
-// router.get("/signup", authController.signup);
-// router.get("/login", authController.login);
-// router.get("/", (req, res, next) => {
-//   res.send("<h1>Hello, World!</h1>")
-// })
-
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
