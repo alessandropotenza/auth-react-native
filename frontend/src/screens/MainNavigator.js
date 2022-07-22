7eadb2e53dfb3aaba98821f5455b9702b039cb67
Alessandro Potenza <potenza1702@gmail.com>
2022-07-22 12:40:59 -0500

    style sign up screen

--- frontend/src/screens/MainNavigator.js
+++ frontend/src/screens/MainNavigator.js
@@ -7,18 +7,32 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./LoginScreen";
 import SignupScreen from "./SignupScreen";
 
+// Component imports
+import BackButton from "../components/BackButton";
+
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ gestureDirection: "vertical" }}
+      >
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{
+            headerTitle: "",
+            headerTransparent: true,
+            headerLeft: () => <BackButton>Back to Log in</BackButton>,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
