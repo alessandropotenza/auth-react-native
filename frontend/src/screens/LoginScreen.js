63c533d420a3f47ff36a47a5967c4e08df725c15
Alessandro Potenza <potenza1702@gmail.com>
2022-07-22 12:49:20 -0500

    store text from input in component state

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -8,8 +8,8 @@ import {
   Keyboard,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 // Component imports
 import CustomInput from "../components/CustomInput";
@@ -18,7 +18,13 @@ import CustomButton from "../components/CustomButton";
 // Color constants import
 import colors from "../util/constants/colors";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailInputHandler = (text) => setEmail(text);
+  const passwordInputHandler = (text) => setPassword(text);
+
   return (
     <LinearGradient
       colors={[colors.pink, colors.lightBlue]}
@@ -28,13 +34,25 @@ const LoginScreen = () => {
         <KeyboardAvoidingView
           style={styles.container}
           behavior={Platform.OS === "ios" && "padding"}
+          keyboardVerticalOffset={-100}
         >
           {/* wrapping Text tag below in plain View fixes choppy KeyboardAvoidingView movements on iOS */}
           <View>
-            <Text style={styles.title}>Login</Text>
+            <Text style={styles.title}>Log in</Text>
           </View>
-          <CustomInput placeholder="Email" icon="person-outline" />
-          <CustomInput placeholder="Password" icon="lock-closed-outline" />
+          <CustomInput
+            placeholder="Email"
+            icon="person-outline"
+            onChangeText={emailInputHandler}
+            value={email}
+          />
+          <CustomInput
+            placeholder="Password"
+            icon="lock-closed-outline"
+            onChangeText={passwordInputHandler}
+            value={password}
+            isSecure={true}
+          />
           <CustomButton style={styles.button}>Login</CustomButton>
 
           {/* Forgot password */}
@@ -45,7 +63,10 @@ const LoginScreen = () => {
           {/* Create account */}
           <View style={styles.createAccountContainer}>
             <Text style={styles.text}>Don't have an account? </Text>
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Signup")}
+              hitSlop={25}
+            >
               <Text style={[styles.text, { color: colors.violet }]}>
                 Sign up!
               </Text>
