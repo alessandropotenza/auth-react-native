72b02cb8e5ad4e6c6154ef51ff51995db50cda33
Alessandro Potenza <potenza1702@gmail.com>
2022-07-22 17:13:45 -0500

    add error handling middleware

---
+++ backend/src/api/middlewares/error.js
@@ -0,0 +1,5 @@
+module.exports = (error, req, res, next) => {
+  const message = error.message;
+  const status = error.statusCode || 500;
+  res.status(status).json({ message });
+};
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const error = require("./api/middlewares/error");
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.json()); //parse JSON data from incoming requests
 
 // import routes
 const authRoutes = require("./api/routes/auth");
 
 // use routes
 app.use(authRoutes);
+
+// error handling middleware
+app.use(error);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
