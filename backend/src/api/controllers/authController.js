c3b351eb7a75708683240be16b59018a16b598a3
Alessandro Potenza <potenza1702@gmail.com>
2022-07-25 15:10:37 -0500

    issue access and refresh tokens on login

--- backend/src/api/controllers/authController.js
+++ backend/src/api/controllers/authController.js
@@ -1,11 +1,12 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // models
 const User = require("../models/User");
+const RefreshToken = require("../models/RefreshToken");
 
 exports.signup = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 12);
   const user = new User(email, hashedPassword);
   try {
@@ -22,19 +23,33 @@ exports.signup = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
   try {
-    const [hashedPasswordData] = await User.getPassword(email);
-    if (!hashedPasswordData[0]) {
+    const [userData] = await User.getUser(email);
+    if (!userData[0]) {
       const err = new Error("User not found");
       err.statusCode = 401;
       throw err;
     }
-    const hashedPassword = hashedPasswordData[0].password;
+    const user = userData[0];
+    const hashedPassword = user.password;
     const passwordsMatch = await bcrypt.compare(password, hashedPassword);
     if (passwordsMatch) {
-      res.status(200).json({ message: "match" });
+      // issue access token
+      const accessToken = jwt.sign(
+        { userId: user.id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "5m" }
+      );
+      // issue refresh token
+      const refreshToken = jwt.sign(
+        { userId: user.id },
+        process.env.REFRESH_TOKEN_SECRET
+      );
+      //add refresh token to database
+      const hashedRefreshToken = await bcrypt.hash(refreshToken, 12);
+      await new RefreshToken(hashedRefreshToken, user.id).addRefresh();
+      res.status(200).json({ accessToken, refreshToken });
     } else {
       const err = new Error("Passwords do not match");
       err.statusCode = 401;
