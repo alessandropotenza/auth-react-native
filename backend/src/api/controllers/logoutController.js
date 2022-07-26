667abc29d239b3e7d40d5b14d5ff54fedd836e46
Alessandro Potenza <potenza1702@gmail.com>
2022-07-26 12:00:57 -0500

    use new RefreshToken model in logout logic

--- backend/src/api/controllers/logoutController.js
+++ backend/src/api/controllers/logoutController.js
@@ -5,19 +5,22 @@ const RefreshToken = require("../models/RefreshToken");
 exports.logout = async (req, res, next) => {
   // get refresh token from POST body
   const refreshToken = req.body.refreshToken;
-  const hashedToken = await bcrypt.hash(refreshToken, 12);
 
   try {
-    const [userIdData] = await RefreshToken.verifyRefresh(hashedToken); // returns user id associated with token
-    // check if refresh token is valid
-    if (!userIdData[0]) {
-      const err = new Error("Invalid Refresh Token");
-      err.statusCode = 401;
-      throw err;
+    //extract userId and iat from claims
+    const base64Payload = refreshToken.split(".")[1];
+    const payload = Buffer.from(base64Payload, "base64").toString("ascii");
+    const { userId, issuedAt } = JSON.parse(payload);
+
+    // check if refresh token exists in database
+    const [claimsData] = await RefreshToken.findClaims(issuedAt, userId);
+    if (!claimsData[0]) {
+      // Refresh token isn't found, still log user out (shouldn't happen)
+      return res.status(200).json({ message: "Logged out successfully" });
     }
 
     // Delete refresh token from database
-    await RefreshToken.revoke(hashedToken);
+    await RefreshToken.revoke(issuedAt, userId);
     res.status(200).json({ message: "Logged out successfully" });
   } catch (err) {
     next(err);
